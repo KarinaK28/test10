ea1b9a442b9f8ba34a3645f59da1405c59275218
Karina <20hs.mm@gmail.com>
2024-10-09 17:45:01 +0200

    19 lesson

--- script.js
+++ script.js
@@ -1,62 +1,22 @@
-'use strict';
+//'use strict';
 
-fetch('./dbHeroes.json')
-    .then(response => response.json())
-    .then(data => practice(data))
-    .catch(err => console.error(err));
-
-
-function practice(response) {
-const allHeroes = [];
-
-response.forEach(item => allHeroes.push(item.name));
-console.log(allHeroes);
+const numberOfFilms = +prompt('How much films have you already seen?', ' ');
+console.log(numberOfFilms);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 }
+ const answ1 = prompt('What is the best Film?', ' ');
+const answ2 = prompt('How much 0-10 point?', ' ');
+const answ3 = prompt('What is the best Film?', ' ');
+const answ4 = prompt('How much 0-10 point?', ' ');
+
+personalMovieDB.movies[answ1] = answ2;
+personalMovieDB.movies[answ3] = answ4;
+
+console.log(personalMovieDB);
 
 
-
-
-
-const mix = [54, 'fff', 55, -5, 'fdfvbgfv'];
-
-mix.forEach(function(item, index, mix){
-    console.log(item, index)
-})
-
-const arr = [1, 2, 3]
-console.log(arr[1]);
-
-const arrObj = {
-    name1: 'AS',
-    1: 'j',
-    '3gg': 'jmjj',
-    5: 'uj'
-};
-
-
-const storeName = 'Lada';
-const storeDescription = {
-    budget: 10000,
-    employees: ['Karina', 'Ivan', 'Anna'],
-    products: {
-        phone: 10000,
-        headset: 200
-    },
-    open: true
-};
-
-
-const test ='f';
-arrObj[test] = 'rrr';
-arrObj.f = 7;
-///console.log(arrObj[dd]);
-//console.log(arrObj);
-console.log(arrObj);
-console.log(arrObj[name1]);
-console.log(arrObj['1']);
-console.log(arrObj['name1']);
-console.log(arrObj['a']);
-console.log(arrObj['3gg']);
-console.log(arrObj.a);
-
-console.log(4 + 5);
