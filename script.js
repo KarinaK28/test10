ee2fa5e9449800b114be4bd849ca87365e5191ce
Karina <20hs.mm@gmail.com>
2024-10-11 09:05:53 +0200

    23 lesson FilmDB_Loop

--- script.js
+++ script.js
@@ -9,13 +9,34 @@ const personalMovieDB = {
     genres: [],
     privat: false
 }
- const answ1 = prompt('What is the best Film?', ' ');
-const answ2 = prompt('How much 0-10 point?', ' ');
-const answ3 = prompt('What is the best Film?', ' ');
-const answ4 = prompt('How much 0-10 point?', ' ');
 
-personalMovieDB.movies[answ1] = answ2;
-personalMovieDB.movies[answ3] = answ4;
+if (personalMovieDB.count < 10) {
+    console.log(`you have seen ${personalMovieDB.count} films `);
+    alert(`you have seen ${personalMovieDB.count} films `);
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log(`you have seen ${personalMovieDB.count} films `);
+    alert(`you have seen a lot of films - ${personalMovieDB.count} `);
+} else if (personalMovieDB.count >= 30){
+    console.log(`you have seen ${personalMovieDB.count} films `);
+    alert(`you are a movie buff lot of films - ${personalMovieDB.count} `);
+} else {
+    console.log(`error `);
+    alert(`error`);
+}
+
+ for (let i = 0; i < 2; i ++) {
+    const answ1 = prompt('What is the best Film111?', ' '),
+          answ2 = prompt('How much 0-10 point?', ' ');
+
+    if (answ1 != null && answ2 != null && answ1 != ' ' && answ2 != ' ' && answ1.length < 50 && answ2.length < 50) {
+        personalMovieDB.movies[answ1] = answ2;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+   
+}
 
 console.log(personalMovieDB);
 
