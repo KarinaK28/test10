b0d921db01bd78c02ee0b10ef4c8f2a39c9498c1
Karyna Karpenko <karina@home.local>
2024-10-14 10:40:19 +0300

    lesson 28 DB Movie with functions

--- script_DBFilm.js
+++ script_DBFilm.js
@@ -1,8 +1,21 @@
 //'use strict';
 
-const numberOfFilms = +prompt('How much films have you already seen?', ' ');
-console.log(numberOfFilms);
-const personalMovieDB = {
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('How much films have you already seen?', ' ');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How much films have you already seen?', ' ');
+        
+    }
+}
+
+start();
+
+
+let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
@@ -10,34 +23,58 @@ const personalMovieDB = {
     privat: false
 }
 
-if (personalMovieDB.count < 10) {
-    console.log(`you have seen ${personalMovieDB.count} films `);
-    alert(`you have seen ${personalMovieDB.count} films `);
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-    console.log(`you have seen ${personalMovieDB.count} films `);
-    alert(`you have seen a lot of films - ${personalMovieDB.count} `);
-} else if (personalMovieDB.count >= 30){
-    console.log(`you have seen ${personalMovieDB.count} films `);
-    alert(`you are a movie buff lot of films - ${personalMovieDB.count} `);
-} else {
-    console.log(`error `);
-    alert(`error`);
+
+
+function writeYourGenres() {
+
+ for (let i = 1; i <= 3; i ++) {
+    personalMovieDB.genres[i -1] = prompt(`What is your favorite genres Number ${i}?`, '');
+}
 }
 
- for (let i = 0; i < 2; i ++) {
-    const answ1 = prompt('What is the best Film111?', ' '),
-          answ2 = prompt('How much 0-10 point?', ' ');
+writeYourGenres();
 
-    if (answ1 != null && answ2 != null && answ1 != ' ' && answ2 != ' ' && answ1.length < 50 && answ2.length < 50) {
-        personalMovieDB.movies[answ1] = answ2;
-        console.log('done');
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i ++) {
+        const answ1 = prompt('What is the best Film?', ' ').trim(),
+              answ2 = prompt('How much 0-10 point?', ' ').trim();
+    
+        if (answ1 != null && answ2 != null && answ1 != '' && answ2 != '' && answ1.length < 5 && answ2.length < 5) {
+            personalMovieDB.movies[answ1] = answ2;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+}
+}
+
+rememberMyFilms() ;
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log(`you have seen ${personalMovieDB.count} films `);
+        alert(`you have seen ${personalMovieDB.count} films `);
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+        console.log(`you have seen ${personalMovieDB.count} films `);
+        alert(`you have seen a lot of films - ${personalMovieDB.count} `);
+    } else if (personalMovieDB.count >= 30){
+        console.log(`you have seen ${personalMovieDB.count} films `);
+        alert(`you are a movie buff lot of films - ${personalMovieDB.count} `);
     } else {
-        console.log('error');
-        i--;
+        console.log(`error `);
+        alert(`error`);
     }
-   
 }
 
-console.log(personalMovieDB);
+detectPersonalLevel();
 
 
+function showMyDB(hidden) {
+    if (!hidden)
+    {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
